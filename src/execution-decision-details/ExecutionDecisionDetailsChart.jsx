411e151eac94e98266a2be05588e2b455ee33c14
Dwayne Hunter <dwaynewhunter@msn.com>
2023-11-12 09:11:48 -0500

    update markers

--- src/execution-decision-details/ExecutionDecisionDetailsChart.jsx
+++ src/execution-decision-details/ExecutionDecisionDetailsChart.jsx
@@ -11,46 +11,62 @@ export default function ExecutionDecisionDetailsChart({ details }) {
   /** @type {import('react').MutableRefObject<import('lightweight-charts').IChartApi>} */
   const chartRef = useRef();
 
-  const candleSticks = useMemo(() => details.stockData.toReversed().map(x => ({
+  const candleSticks = useMemo(() => details.bars.toReversed().map(x => ({
     time: Date.parse(x.date) / 1000,
     ...x,
-  })), [details.stockData])
+  })), [details.bars])
 
-  const volumeData = useMemo(() => details.stockData.toReversed().map(x => ({
+  const volumeData = useMemo(() => details.bars.toReversed().map(x => ({
     time: Date.parse(x.date) / 1000,
     value: x.volume || 0,
-  })), [details.stockData])
+  })), [details.bars])
 
-  const shortEMAs = useMemo(() => details.shortEMA
+  const shortEMAs = useMemo(() => details.indicators.shortEMA
     .filter(x => !!x.ema)
     .map(x => ({
       time: Date.parse(x.date) / 1000,
       value: x.ema || 0,
-    })), [details.shortEMA])
+    })), [details.indicators.shortEMA])
 
-  const longEMAs = useMemo(() => details.longEMA
+  const longEMAs = useMemo(() => details.indicators.longEMA
     .filter(x => !!x.ema)
     .map(x => ({
       time: Date.parse(x.date) / 1000,
       value: x.ema || 0,
-    })), [details.longEMA])
+    })), [details.indicators.longEMA])
 
-  const vwma = useMemo(() => details.volumeWeightedAvgPrice
+  const vwma = useMemo(() => details.indicators.volumeWeightedAvgPrice
     .filter(x => !!x.vwap)
     .map(x => ({
       time: Date.parse(x.date) / 1000,
       value: x.vwma || 0,
-    })), [details.volumeWeightedAvgPrice])
+    })), [details.indicators.volumeWeightedAvgPrice])
+
+  // const macd = useMemo(() => details.executionDecision.movingAverageConvergenceDivergence
+  //   .filter(x => !!x.macd))
 
   const markers = useMemo(() => [
-    {
-      time: Date.parse(details.executionDecision.signalTime) / 1000,
-      position: 'aboveBar',
-      color: '#f68410',
-      shape: 'arrowDown',
-      text: `${details.executionDecision.action} Signal`,
-    },
-  ], [details.executionDecision.signalTime, details.executionDecision.action])
+    // {
+    //   time: Date.parse(details.executionDecision.signalTime) / 1000,
+    //   position: 'aboveBar',
+    //   color: '#f68410',
+    //   shape: 'arrowDown',
+    //   text: `${details.executionDecision.action} Signal`,
+    // },
+    ...details.indicators.pivots
+      .filter(x => !!x.highTrend || !!x.lowTrend)
+      .map(x => ({
+        time: Date.parse(x.date) / 1000,
+        position: x.highTrend ? 'aboveBar' : 'belowBar',
+        color: x.highTrend ? '#4bffb5' : '#ff4976',
+        shape: x.highTrend ? 'arrowDown' : 'arrowUp',
+        text: x.highTrend || x.lowTrend,
+      })),
+  ], [
+    details.executionDecision.signalTime,
+    details.executionDecision.action,
+    details.indicators.pivots,
+  ])
 
   useEffect(
     () => {
@@ -74,19 +90,6 @@ export default function ExecutionDecisionDetailsChart({ details }) {
       })
       chartRef.current.timeScale().fitContent()
 
-      const candleSeries = chartRef.current.addCandlestickSeries({
-        upColor: '#4bffb5',
-        downColor: '#ff4976',
-        borderDownColor: '#ff4976',
-        borderUpColor: '#4bffb5',
-        wickDownColor: '#838ca1',
-        wickUpColor: '#838ca1',
-      })
-
-      candleSeries.setData(candleSticks)
-
-      candleSeries.setMarkers(markers)
-
       const volumeSeries = chartRef.current.addHistogramSeries({
         color: '#182233',
         lineWidth: 2,
@@ -102,6 +105,19 @@ export default function ExecutionDecisionDetailsChart({ details }) {
       });
 
       volumeSeries.setData(volumeData)
+
+      const candleSeries = chartRef.current.addCandlestickSeries({
+        upColor: '#4bffb5',
+        downColor: '#ff4976',
+        borderDownColor: '#ff4976',
+        borderUpColor: '#4bffb5',
+        wickDownColor: '#838ca1',
+        wickUpColor: '#838ca1',
+      })
+
+      candleSeries.setData(candleSticks)
+
+      candleSeries.setMarkers(markers)
 
       const shortEMASeries = chartRef.current.addLineSeries({
         color: '#4bffb5',
